ae59b6a4c3bcd25428bee229a0dc762d7d39b898
kacperchm <chmuraka1998@gmail.com>
2023-06-04 09:39:57 +0200

    Initial commit

---
+++ src/main/java/com/kacperchm/jwdcolors/JwdcolorsApplication.java
@@ -0,0 +1,13 @@
+package com.kacperchm.jwdcolors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdcolorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwdcolorsApplication.class, args);
+    }
+
+}
